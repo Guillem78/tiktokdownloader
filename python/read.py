ee8ced3d3e5c47d7141dab60662909d85ddab435
dev <guillemba@gmail.com>
2022-12-31 01:25:33 +0000

    wip

--- python/read.py
+++ python/read.py
@@ -1,11 +1,14 @@
-import pandas
 # Using readlines()
-# file1 = open("C:\Users\bonamusg\developer\tiktokdownloader\src\urls.conf", "r")
-pandas.read_csv(r"C:\Users\bonamusg\developer\tiktokdownloader\src\urls.conf")
+file1 = open("/srv/tiktokdownloader/src/urls.conf", "r")
 Lines = file1.readlines()
 
 count = 0
 # Strips the newline character
+mydata = ['urls']
 for line in Lines:
     count += 1
+    mydata.append("Line{}: {}".format(count, line.strip()))
     print("Line{}: {}".format(count, line.strip()))
+
+print (" Paco")
+print ("Data is: ", mydata )
